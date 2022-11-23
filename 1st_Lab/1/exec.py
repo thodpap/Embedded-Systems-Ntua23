6c3ba40e971331eb6ec9255e5b5c4837681f764c
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-11-23 23:38:46 +0200

    improve results

--- 1st_Lab/1/exec.py
+++ 1st_Lab/1/exec.py
@@ -5,7 +5,7 @@ print(subprocess.run(['gcc', 'phods_opt_bx_by.c', '-O0', '-o', 'phods_opt_bx_by'
 a_176 = [1, 2, 4, 8, 11, 16, 22, 44, 88, 176]
 a_144 = [1, 2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 36, 48, 72, 144]
 
-N = 30
+N = 50
 
 f = open("results_phods_opt_bx_by.txt", 'w')
 
@@ -19,6 +19,7 @@ for i in a_144:
         min_time = 1e9
 
         mean_time = 0 
+        c = 0
         
         for _ in range(N):
 
@@ -27,12 +28,13 @@ for i in a_144:
             result = subprocess.run(['./phods_opt_bx_by ' + string], stdout=subprocess.PIPE, shell=True)
             
             time = int(result.stdout.decode('utf-8').replace("\n", ""))
+            if time > 0:
+                max_time = max(time, max_time)
+                min_time = min(time, min_time)
+                mean_time += time
+                c += 1
 
-            max_time = max(time, max_time)
-            min_time = min(time, min_time)
-            mean_time += time
-
-        mean_time /= N
+        mean_time /= c
 
         f.write("\n")
         f.write(f"   mean time = {mean_time}\n")
--- 1st_Lab/1/exec_first.py
+++ 1st_Lab/1/exec_first.py
@@ -3,24 +3,23 @@ import subprocess
 max_time = -1
 min_time = 1e9
 
-N = 30
-
+N = 50
+c = 0
 mean_time = 0 
 print(subprocess.run(['gcc', 'phods_opt.c', '-O0', '-o', 'phods_opt'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
 for i in range(N):
-    
-    print(i, "th loop")
-
+    # print(i, "th loop")
     result = subprocess.run(['./phods_opt'], stdout=subprocess.PIPE)
     time = int(result.stdout.decode('utf-8').replace("\n", ""))
 
-    print("current time =", time)
-
-    max_time = max(time, max_time)
-    min_time = min(time, min_time)
-    mean_time += time
-
-mean_time /= N
+    # print("current time =", time)
+    if time > 0:
+        max_time = max(time, max_time)
+        min_time = min(time, min_time)
+        mean_time += time
+        c += 1
+        
+mean_time /= c
 
 print()
 

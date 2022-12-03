daf28173aa6eb1ebace3edc83302ec5363166c71
Orfeasfil <orfeasfil2000@gmail.com>
2022-12-03 22:24:15 +0200

    DRR completed with python scripts

---
+++ 2on_lad/DDTR/DRR/python_run_valgrind_mem_accesess.py
@@ -0,0 +1,22 @@
+import subprocess as sub
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('mem_acesses_logger')
+
+names = ['sll', 'dll', 'dyn_arr']
+
+total = len(names) * len(names)
+counter = 1
+
+for i in names:
+    for j in names:
+
+        sub.run(f"touch /mem_accesses_results/\"mem_accesses_log_{i}_{j}.txt", shell=True, stdout=sub.PIPE).stdout.decode('utf-8')
+
+        command = f"valgrind --log-file=./mem_accesses_results/\"mem_accesses_log_{i}_{j}.txt\" --tool=lackey --trace-mem=yes ./D_binaries/drr_{i}_{j}"
+
+        logger.info(sub.run(command, shell=True, stdout=sub.PIPE).stdout.decode('utf-8'))
+
+        logger.info(str(counter) + " done out of " + str(total))
+        counter += 1

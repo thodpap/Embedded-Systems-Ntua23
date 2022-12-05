6194767469416b09c259c4a5941323107edd3fa6
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-12-05 17:11:55 +0200

    fix segmentation error

--- 2nd_lad/DDTR/dijsktra/run_and_print.py
+++ 2nd_lad/DDTR/dijsktra/run_and_print.py
@@ -9,5 +9,4 @@ names = ['dijkstra', 'dijkstra_dynamic_sll', 'dijkstra_dynamic_dll', 'dijkstra_d
 outs = ['dijkstra.txt', 'dijkstra_dynamic_sll.txt', 'dijkstra_dynamic_dll.txt', 'dijkstra_dynamic_dyn_arr.txt']
 for i, name in enumerate(names):
     sub.run(f"touch ./correctness/{outs[i]}", shell=True, stdout=sub.PIPE).stdout.decode('utf-8')
-
-    logger.info(sub.run(f"./binaries/{name} input.dat ./correctness/{outs}", shell=True, stdout=sub.PIPE).stdout.decode('utf-8'))
+    logger.info(sub.run(f"./binaries/{name} input.dat ./correctness/{outs[i]}", shell=True, stdout=sub.PIPE).stdout.decode('utf-8'))
